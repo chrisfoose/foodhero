07449e631a613cae39ec1dea40630a1ffd534374
chrisfoose <47850167+chrisfoose@users.noreply.github.com>
2023-03-06 20:56:58 -1000

    trying to get API to work

--- foodhero.js
+++ foodhero.js
@@ -1,65 +1,24 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '614aaf1a46mshd0cce11497fe7a0p1e0622jsn8af5d0d1a5b9',
+    method: "GET",
+    headers: {
+        'X-RapidAPI-Key': '614aaf1a46mshd0cce11497fe7a0p1e0622jsn8af5d0d1a5b9',
 		'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
-	}
+    }
 };
 
-const getData = async () => {
-    try {
-        url="https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple"
-        const response = await fetch(url);
-        fetch('https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple', options)
-	    .then(response => response.json())
-	    .then(response => console.log(response))
-	    .catch(err => console.error(err));
-        const food = response.json();
-        return food.map(function getFood() {
-            let userInput = document.getElementById("userInput");
-            return `
-            <div id="results">
-            {$food}
-            </div>
-            `
-        });
+document.getElementById("sendFood").addEventListener("click", async (e) =>) {
+    const inputVal = document.getElementById("userinput").ariaValueMax;
+    const res = await fetch(
+        'https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=$(inputval)',
+        options
+    );
 
-    } catch (error) {
-        console.log(error);
-        alert('Error');
-    }
-}
-
-fetch('https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-    
-    function getFood(userInput) {
-        userInput = document.getElementById("userInput");
-       for (i= 0; i = food; i++ ) {
-
-       
-                return `
-                <div id="results">
-                ${food[i]};
-
-                </div>
-                `
-            }
-
-    };
-
-    // function findFood() {
-    //     if (userInput != "") {
-    //         return `
-    //         <div id="results">
-    //         ${getData.food}
-    //         </div>
-    //         `
-    //     }
-
-
-
-
+    const resJson = await res.json();
+    const listParent = document.getElementyById('results');
+    listParent.innerHTML = "";
+    resJson.hints.forEach((hint) => {
+        const item = document.createElement("li");
+        item.innerHTML = hint.food.label;
+        listParent.appendChildren(item);
+    });
+};
